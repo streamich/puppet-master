ddc800c535904c0b09c07308c70d725cebab1361
streamich <streamich@gmail.com>
2018-09-21 15:56:38 +0200

    test: 💍 add a test

--- test.js
+++ test.js
@@ -6,4 +6,11 @@ execute({
   func,
   args: [1, 2],
   module: __dirname + '/module.js',
-}).then(console.log, console.error);
+}).then((res) => {
+  if (res !== 3) {
+    console.error('expected 3, received ' + res);
+    process.exit(1);
+  } else {
+    console.log('Test passed!');
+  }
+}, () => process.exit(1));
